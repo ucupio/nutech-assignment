cbf83e07bf5f572ca6963e1bd931c1556e27ff4b
Maulana Yusuf Azmi <ucupio@gmail.com>
2023-06-13 12:40:41 +0700

    enable cors

--- apps/nutech/src/main.ts
+++ apps/nutech/src/main.ts
@@ -12,6 +12,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  })
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
